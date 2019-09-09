352225ac396ba0fb73d80aa84fa38705fab06dc1
bmalumphy <bryanmalumphy@gmail.com>
2019-09-09 16:32:59 -0400

    Initial Commit

---
+++ Swing Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swing Data
+//
+//  Created by Bryan Malumphy on 9/9/19.
+//  Copyright © 2019 Math and Coffee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
